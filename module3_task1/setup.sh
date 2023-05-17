7c32f3e545fa5037da6f0b786485d1f33c81b299
chrs-creyk <chrisacount117@gmail.com>
2023-05-17 22:39:07 +0200

    make build

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-sudo apt-get update && apt-get install -y hugo make
+sudo apt-get update && apt upgrade -y
+sudo apt-get install -y hugo make
 sudo apt-get install curl -y
 sudo curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+sudo wget https://github.com/golangci/golangci-lint/releases/download/v1.52.2/golangci-lint-1.52.2-darwin-amd64.tar.gz
 sudo apt install golang
 sudo go install github.com/gohugoio/hugo@latest
 sudo apt-get install ./install_hugo.deb
 sudo npm install -g markdownlint-cli
-sudo apt update
-sudo go get -u github.com/golangci/golangci-lint/cmd/[email protected]
+sudo make build
