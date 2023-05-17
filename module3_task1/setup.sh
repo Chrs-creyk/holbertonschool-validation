23eafda65e76b68b0603c8e6acd6a32c616c1b32
chrs-creyk <chrisacount117@gmail.com>
2023-05-17 07:40:19 +0200

    sin

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -3,4 +3,3 @@ sudo apt-get update && apt-get install -y hugo make
 sudo apt-get install curl -y
 sudo curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 sudo apt-get install ./install_hugo.deb
-export PATH=$PATH:$/go GOPATH/bin
