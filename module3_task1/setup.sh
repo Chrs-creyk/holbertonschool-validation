7712df4489d56665b2d3dd92f21d18621ebe80a0
chrs-creyk <chrisacount117@gmail.com>
2023-05-17 08:33:33 +0200

    cambios

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -3,3 +3,7 @@ sudo apt-get update && apt-get install -y hugo make
 sudo apt-get install curl -y
 sudo curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 sudo apt-get install ./install_hugo.deb
+sudo npm install -g markdownlint-cli
+sudo apt update
+sudo go install github.com/golangci/golangci-lint/cmd/[email protected]`
+sudo apt install golang
