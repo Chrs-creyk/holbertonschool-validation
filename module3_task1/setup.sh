5e8287523d01ea80262622302bf011317024791e
chrs-creyk <chrisacount117@gmail.com>
2023-05-17 23:22:26 +0200

    9.9

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -4,6 +4,6 @@ sudo apt install -y wget make dpkg curl
 sudo wget -O hugo_binary.deb https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.deb
 sudo dpkg -i hugo_binary.deb
 sudo rm hugo_binary.deb
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sudo sh -s -- -b $(go env GOPATH)/bin
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sudo sh -s -- -b /usr/local/bin
 sudo npm install -g markdown-link-check
 sudo npm install -g markdownlint-cli
